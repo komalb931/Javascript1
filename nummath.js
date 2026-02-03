8fa08f52261e914fbaa3e0b073904874e04a566a
komalb931 <118887981+komalb931@users.noreply.github.com>
2026-02-03 11:10:31 +0000

    Math and Num

---
+++ nummath.js
@@ -0,0 +1,33 @@
+// ++++++++++++++ number++++++++++++++++
+
+// const score= 400;
+// console.log(score);
+// const balence= new Number(123)
+// console.log(balence);
+// console.log(balence.toString().length)
+// console.log(balence.toFixed(2));// using digit
+
+// const num = 123.895
+// console.log(num.toPrecision(4));//
+// const num2=10000000
+// console.log(num2.toLocaleString('en-In')); // comma
+
+
+//+++++++++++++ Math ++++++++++++++++
+
+
+console.log(Math.abs(-9));// alwase positive value 
+console.log(Math.round(4.3));
+console.log(Math.max(2,4,5,3));
+console.log(Math.min(2,4,5,3));
+
+
+const min =10
+const max =20
+console.log(Math.floor(Math.random()* (max-min+1))+max);//random star for 0 
+//but that time not star for 0 using +1
+
+
+
+
+
