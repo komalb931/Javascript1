984a6dc8c94a6e2006dc437da8f9ee12edb14dd3
komalb931 <118887981+komalb931@users.noreply.github.com>
2026-02-06 08:35:41 +0000

    function

---
+++ function.js
@@ -0,0 +1,16 @@
+function addTowNum(num1,num2){
+    // console.log("komal");
+    // console.log(num1+num2);
+    return(num1+num2)
+    
+}
+const result=addTowNum(2,4)
+console.log(result);
+/// array 
+const myarr =[111,33,333,55,444,]
+
+function handleArray(array){
+    return array[3];
+
+}
+console.log(handleArray(myarr));
