2adb2baa896e38863010ce0316f2379f73e6090e
komalb931 <118887981+komalb931@users.noreply.github.com>
2026-02-07 10:51:14 +0000

    loop

---
+++ for.js
@@ -0,0 +1,22 @@
+// // for (let i = 0; i <= 10; i++) {
+// //     const element = i;
+// //     console.log(element);
+    
+// // }
+
+// for (let i = 0; i <=10; i++) {
+//     console.log(`outer loop ${i}`);
+    
+//     for (let j = 0; j <=10; j++) {
+//         // console.log(`inner loop ${j}`);
+    
+//         console.log(i+'*'+ j+'='+i*j);
+        
+//     }
+    
+// }
+let i=0;
+while (i<=3) {
+    console.log(`print here ${i}`);
+   i=i+3;
+}
