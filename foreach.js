21049a56c6e05aa577d482150cb665956ef6b212
komalb931 <118887981+komalb931@users.noreply.github.com>
2026-02-09 11:08:26 +0000

    foreach

---
+++ foreach.js
@@ -0,0 +1,16 @@
+const coding =["c","CPP","JAVA","Python"];
+// coding.forEach((val)=>{
+//    console.log(val);
+   
+    
+// })
+
+coding.forEach((item,index,val)=>{
+//    console.log(item,index,val);
+   
+    
+})
+
+const num=[1,2,3,4,5,6,7,8,9,10]
+let mynum=num.filter((num)=>num<2)
+console.log(mynum);
