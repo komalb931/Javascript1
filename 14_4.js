a54a74672c71af9fe41dc5672e36c4ba71fdd590
komalb931 <118887981+komalb931@users.noreply.github.com>
2026-04-14 11:12:19 +0000

    if-else question

---
+++ 14_4.js
@@ -0,0 +1,49 @@
+// 1)
+let age=18;
+if(age >= 18){
+    // console.log("Eligible");  
+}
+else{
+    //   console.log("Not Eligible");  
+}
+
+// 3)
+let num = 10;
+if(num % 2 === 0){
+    // console.log("even");   
+}
+else if(num>=0){
+    //  console.log("positve")
+}
+else{
+    // console.log("nagative")
+}
+
+// 4)
+let a = 10, b = 25, c = 15;
+if(a>b){
+    // console.log("a larget");
+}
+else if(b>c){
+    // console.log("b largest");
+    
+}
+else if(c>a){
+    // console.log("c largest");
+    
+}
+else{
+    // console.log("no any largest");
+    
+}
+
+// 5)
+let year = 2024;
+if(year%4===0){
+    console.log("this year leap year");
+    
+}
+else{
+    console.log("This is not leap year");
+    
+}
