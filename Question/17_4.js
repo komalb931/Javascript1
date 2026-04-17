69abcd16bf4c3f4a9c04d898fb95aef219baf9a0
komalb931 <118887981+komalb931@users.noreply.github.com>
2026-04-17 11:57:02 +0000

    for_loop question

---
+++ Question/17_4.js
@@ -0,0 +1,31 @@
+// 1)
+for (let i = 1; i <=10; i++) {
+    // console.log(`${i}`);  
+}
+
+// 2)
+for (let i = 1; i <=10; i++) {
+    if(i%2===0){
+//   console.log(`${i}`);
+    }  
+    
+}
+// 3)
+for (let i = 10; i >= 1; i--) {
+    // console.log(i);
+}  
+
+//4)
+for (let i = 1; i <= 10; i++) {
+    // console.log(`5 * ${i} = ${5 * i}`);
+}
+
+//5)
+
+let sum = 0;
+
+for (let i = 1; i <= 10; i++) {
+    sum = sum + i;
+}
+
+console.log(sum); // 55
