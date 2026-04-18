c71d6e7b1d368ef96ad0b57385b4c810e15196fd
komalb931 <118887981+komalb931@users.noreply.github.com>
2026-04-18 10:16:11 +0000

    while loop quetion

--- Question/17_4.js
+++ Question/17_4.js
@@ -28,4 +28,4 @@ for (let i = 1; i <= 10; i++) {
     sum = sum + i;
 }
 
-console.log(sum); // 55
+console.log(sum); 
---
+++ Question/18_4.js
@@ -0,0 +1,45 @@
+// 1)
+let i = 1;
+
+while (i <= 10) {
+    // console.log(i);
+    i++   }
+
+    // 2)
+
+    let j = 10;
+
+while (j >= 1) {
+    // console.log(j);
+    j-- ; }
+    // 3)
+
+      let a = 1;
+
+while (a <= 10) {
+
+    if(a%2===0){
+        //  console.log(a);
+    }
+    a++; }
+
+// 4)
+let b=1;
+let sum =0;
+while(b<=10){
+    sum=b+sum
+    // console.log(sum);
+    
+ b++;
+}
+// 5) factorial no
+let num = 5;
+let fact = 1;
+let c = 1;
+
+while (c <= num) {
+    fact = fact * c;
+    c++;
+}
+
+console.log(fact); // 120
