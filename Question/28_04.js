2b57dbf73d68fb7c0ebf2d7e09e419e0fde43a85
komalb931 <118887981+komalb931@users.noreply.github.com>
2026-04-28 10:29:41 +0000

    arrow function

---
+++ Question/28_04.js
@@ -0,0 +1,19 @@
+const geet=()=>{
+ console.log("Hello");
+    
+} 
+
+const addTow=(num1,num2)=>{
+return num1+num2;
+}
+ console.log(addTow(2,4));
+
+const squre=(num)=>(num*num)
+  console.log(squre(2));
+
+//   
+const arr = [1, 2, 3, 4];
+
+const even = arr.filter(num => num % 2 === 0);
+
+console.log(even); 
