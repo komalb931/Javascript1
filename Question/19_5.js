ab5f393c537debdcb11b9296922fcdd4ade6d594
komalb931 <118887981+komalb931@users.noreply.github.com>
2026-05-19 00:28:36 +0000

    object

---
+++ Question/19_5.js
@@ -0,0 +1,19 @@
+const student={
+    name:"komal arun borate",
+    age:25,
+    city:"Baramati"
+}
+console.log(student)
+
+const user = {
+  name: "Komal",
+  age: 24
+}
+ console.log(user.name)
+console.log(user["name"])
+
+user.age=25
+console.log(user)
+
+user.email="komal@931gmail.com"
+console.log(user)
